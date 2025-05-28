fcd5db8d1929ed6b19472736dde7be48a90d63e5
mattias421 <mcross2@sheffield.ac.uk>
2025-05-28 15:24:02 +0100

    update hparams and lr sweep

--- xps/lr_sweep.sh
+++ xps/lr_sweep.sh
@@ -9,10 +9,15 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
 #SBATCH --output=results/slurm/%x-%a.out
-#SBATCH --array=1
+#SBATCH --array=0-9
+
+LR=(1e-1 1e-2 1e-3 1e-4 1e-5 2e-1 2e-2 2e-3 2e-5 2e-4) 
+export WANDB_TAGS=lr_sweep
+
+lr=${LR[$SLURM_ARRAY_TASK_ID]}
 
 module load CUDA/12.4.0 
 
 source ./.venv/bin/activate
 
-python train.py hparams/train.yaml --trial_id=alpha_bs4 --batch_size=4 --number_of_epochs=100
+python train.py hparams/train.yaml --trial_id=lr_$lr --evaluate=false --lr_whisper=$lr --number_of_epochs=5
